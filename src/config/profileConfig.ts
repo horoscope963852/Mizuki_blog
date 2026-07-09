b24c9f4927180ab1572b6c0b21f2a7473db09759
Mao Qingshan <151914978+horoscope963852@users.noreply.github.com>
2026-07-09 20:18:09 +0800

    更新个人签名

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -4,7 +4,7 @@ import type { ProfileConfig } from "../types/config";
 export const profileConfig: ProfileConfig = {
   avatar: "assets/images/avatar.webp", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
   name: "horoscope",
-  bio: "不做无法实现的梦",
+  bio: "若批评不自由则赞美无意义",
   typewriter: {
     enable: true, // 启用个人简介打字机效果
     speed: 80, // 打字速度（毫秒）
